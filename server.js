2f8ede0bd9610c5681465ca067ac3cd9fe7109d2
Bo Latt <bolattny@gmail.com>
2022-09-14 01:54:43 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const axios = require("axios");
+const url =
+  "https://newsapi.org/v2/top-headlines?country=us&apiKey=43f35ad711e440ec977ce87079f2a215";
+
+app.get("/", (req, res) => {
+  const id = req.query.id;
+  //   axios.get(url).then((response) => {
+  //     res.send(JSON.stringify(response.data.articles));
+  //   });
+  res.send(`id:${id}`);
+});
+
+const PORT = process.env.PORT || 8080;
+
+app.listen(PORT, () => console.log(`server running on ${PORT}`));
