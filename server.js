6a9f445e3716cf42c69fd781ea24c76a615a590d
Bo Latt <bolattny@gmail.com>
2022-09-15 19:14:33 -0400

    replace spaces with percent

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
-let url =
-  "https://newsapi.org/v2/top-headlines?country=us&apiKey=43f35ad711e440ec977ce87079f2a215";
+const apiKey1 = "9a3c6ce90d4b4958a771bfc5370df6b1";
+const apiKey2 = "43f35ad711e440ec977ce87079f2a215";
+let url = `https://newsapi.org/v2/top-headlines?country=us&apiKey=${apiKey2}`;
 
 const app = express();
 
@@ -11,10 +12,12 @@ app.use(cors());
 app.get("/", (req, res) => {
   const keyword = req.query.keyword;
   if (keyword) {
+    keyword = keyword.replaceAll(" ", "%");
+    console.log(keyword);
     // url =
     //   input +
     //   "&sortBy=popularity&searchIn=title&pageSize=21&apiKey=43f35ad711e440ec977ce87079f2a215";
-    url = `https://newsapi.org/v2/everything?q=${keyword}&sortBy=popularity&searchIn=title&pageSize=21&apiKey=43f35ad711e440ec977ce87079f2a215`;
+    url = `https://newsapi.org/v2/everything?q=${keyword}&sortBy=popularity&searchIn=title&pageSize=21&apiKey=${apiKey1}`;
   }
   console.log("input is", keyword);
   console.log("url is", url);
